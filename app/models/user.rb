042b6f34d85b5927d04675c8e32c9b5b49e9e6e6
tomoaki-kimura <tkim19750404@gmail.com>
2022-04-26 06:24:32 +0900

    issue #23 introductionカラムにバリデーションが設定されていない

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,9 +8,8 @@ class User < ApplicationRecord
   has_one_attached :profile_image
 
   validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
+  validates :introduction, presence: true
 
-  
-  
   def get_profile_image
     (profile_image.attached?) ? profile_image : 'no_image.jpg'
   end
