42cf21d2ae79529ee8d4076ccf0022e15b97ab91
tomoaki-kimura <tkim19750404@gmail.com>
2022-04-26 06:25:48 +0900

    issue #24 他ユーザーのプロフィールを編集できないようにする

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :ensure_correct_user, only: [:update]
+  before_action :ensure_correct_user, only: [:edit, :update, :destroy]
 
   def show
     @user = User.find(params[:id])
